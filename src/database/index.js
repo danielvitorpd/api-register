ec4dc3e1fac22ad1579e4d2af3a4ffeb792f75f5
Daniel Vitor Pessoa Damião <danielvitor@usp.br>
2021-09-01 19:34:34 -0300

    config heroku port

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
+
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/noderest', { useMongoClient: true });
+mongoose.connect(
+    process.env.MONGO_URL,
+    { usenewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -8,4 +10,4 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 require('./controllers/authController')(app);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
